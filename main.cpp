5319cc6dc4fbaa11d90ee6cddfe49f5f423f225d
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-07-25 13:37:30 +0200

    comments and variables

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    // comments
+    /*
+    
+    multiriga comment
+
+    */
+
+   int eta;
+   eta = 40;
+
+
+    std::cout << "ciao sono Isuruuuuuuuuuuuuuuuu e ho " << eta <<" anni" << std::endl;
+    std::cout << "prova";
+    return 0;
+}
