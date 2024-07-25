e12babf98d986b1fdd35945faa39da610221ea9a
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-07-25 14:17:38 +0200

    data types

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,7 +8,25 @@ namespace Prova {
     int cedo = 0;
 }
 
+using std::string;
+
 int main() {
+
+    // std::string nome = "Isuru";
+    string nome  = "Isuru";
+    int numeri[5] = {10, 20, 30};
+
+    std::cout << numeri;
+
+    // pointers
+    int* puntatore = numeri;
+
+    struct Persona
+    {
+        string nome;
+        string cognome;
+        string codiceFiscale;
+    }
 
     // comments
     /*
@@ -27,7 +46,12 @@ int main() {
    cedo;
 
 
-    cout << "ciao sono Isuruuuuuuuuuuuuuuuu e ho " << eta <<" anni" << std::endl;
+    cout << "ciao sono " << nome  << " e ho " << eta <<" anni" << std::endl;
     std::cout << "prova";
     return 0;
 }
+
+// int, float (7 numbers after the decimentl) double (15 numbers after the decimentl)
+// char bool
+// string
+// puntatori array structure classi
