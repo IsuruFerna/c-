a149fb2b0684f634c0d92aaec4ab31221ca74444
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-07-25 14:07:24 +0200

    namespace

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
+namespace Prova {
+    int count = 0;
+    int cedo = 0;
+}
+
 int main() {
 
     // comments
@@ -9,11 +16,18 @@ int main() {
 
     */
 
+   using namespace Prova;
+
    int eta;
    eta = 40;
 
+   const int ETA_MASSIMA = 120;
 
-    std::cout << "ciao sono Isuruuuuuuuuuuuuuuuu e ho " << eta <<" anni" << std::endl;
+   Prova::count;
+   cedo;
+
+
+    cout << "ciao sono Isuruuuuuuuuuuuuuuuu e ho " << eta <<" anni" << std::endl;
     std::cout << "prova";
     return 0;
 }
